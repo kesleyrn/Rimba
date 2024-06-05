a38cd029f1289e2926fe40f41930c322698aff82
Kesle Yrn <kesachyrn@gmail.com>
2024-06-05 18:27:19 +0200

    Updating Titlename

--- admin/desheader.php
+++ admin/desheader.php
@@ -12,7 +12,7 @@ header('location:login.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../images/RimbaTradenarkt.png" type="image/x-icon">
-    <title>Rimba &mdash; HighQuality</title>
+    <title>Rimba &mdash; Shop</title>
     <link rel="stylesheet" href="admincss/desheader.css">
     
 </head>
